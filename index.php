38480ccafc67fb962e9f99554aa3d37ffb74fe68
BlaviButcher <rileydouglas@outlook.com>
2021-09-16 13:09:12 +1200

    :construction: index.php base html

--- index.php
+++ index.php
@@ -6,6 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patient Portal</title>
 </head>
-<body>
+<body> 
+    <div id="card">
+        <h1>Patient Portal</h1>
+        <form action="/php/sofa.php" method="post">
+            <label for="NHI-input">NHI</label>
+            <input type="text" name="NHI" id="NHI-input">
+            <label for="fname-input">First Name</label>
+            <input type="text" name="fname" id="fname-input">
+            <label for="lname-input">Last Name</label>
+            <input type="text" name="lname" id="lname-input">
+            <button type="submit">Submit</button>
+        </form>
+        <p>The sequential organ failure assessment score (SOFA score), previously known as the sepsis-related organ failure assessment score,
+            is used to track a person's status during the stay in an intensive care unit (ICU) to determine the extent of a person's organ function 
+            or rate of failure. The score is based on six different scores, one each for the respiratory, cardiovascular, hepatic, 
+            coagulation, renal and neurological systems.</p>
+    </div>
 </body>
 </html>
