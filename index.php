84c2fc505ebeb942c608bd18da9d1b02d4745d49
BlaviButcher <rileydouglas@outlook.com>
2021-09-16 22:25:10 +1200

    push

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patient Portal</title>
     <link rel="stylesheet" href="css/index.css">
+
+    <!-- font-family: "Lato", sans-serif; -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,400&display=swap"
+        rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
@@ -22,16 +33,16 @@
         <h2>Enter Credentials</h2>
         <form action="php/sofa.php" method="post">
             <div class="input-wrap">
+                <input type="text" name="NHI" id="NHI-input" required>
                 <label for="NHI-input">NHI</label>
-                <input type="text" name="NHI" id="NHI-input">
             </div>
             <div class="input-wrap">
+                <input type="text" name="fname" id="fname-input" required>
                 <label for="fname-input">First Name</label>
-                <input type="text" name="fname" id="fname-input">
             </div>
             <div class="input-wrap">
+                <input type="text" name="lname" id="lname-input" required>
                 <label for="lname-input">Last Name</label>
-                <input type="text" name="lname" id="lname-input">
             </div>
             <div id="submit-wrap">
                 <button type="submit">Submit</button>
