ae77a8237e7e43053fb6543301891e77f4680ff5
BlaviButcher <rileydouglas@outlook.com>
2021-09-27 17:22:50 +1300

    :construction: added logic to get inputs

--- php/results.php
+++ php/results.php
@@ -1,16 +1,26 @@
 <?php 
-    var_dump($_POST);
-    $respiratoryRanges = array( 
-        array (
-           "mmHg" => 400,
-           "kPa" => 53.3,	
-        ),
-        
-        array (
+   var_dump($_POST);
+   $respiratoryScore = getRespiratoryScore();
+   $glasgowScore = getGlasgowComaInput();
+   $cardioVascularScore = getCardioVascularInput();
+   $liverScore = getLiverInput();
+   $plateletScore = getPlateletInput();
+   $creatineScore = getCreatineInput();
+
+   function getRespiratoryScore() {
+      //  array of the ranges that the respiratory could fall into. 
+      // incudes the alternative units
+      $respiratoryRanges = array( 
+         array (
             "mmHg" => 400,
             "kPa" => 53.3,	
          ),
-        
+         
+         array (
+            "mmHg" => 400,
+            "kPa" => 53.3,	
+         ),
+         
          array (
             "mmHg" => 300,
             "kPa" => 40,	
@@ -19,64 +29,96 @@
          array (
             "mmHg" => 200,
             "kPa" => 26.7,	
-         ),
+         ),      
 
          array (
             "mmHg" => 100,
             "kPa" => 13.3,	
          )
-     );
+      );
+      // get the respiratory input and units used
+      $respiratoryInput = $_POST["respiratory-figures"];
+      $respiratoryUnits = $_POST["respiratory-units"];
+      // convert string to bool
+      $isVentilated = filter_var($_POST["ventilation-input"], FILTER_VALIDATE_BOOLEAN);
 
-     $respiratoryInput = $_POST["respiratory-figures"];
-     $respiratoryUnits = $_POST["respiratory-units"];
-     // convert post string to bool
-     $isVentilated = filter_var($_POST["ventilation-input"], FILTER_VALIDATE_BOOLEAN);
+      // initialise score to -1 for error handling
+      $respiratoryScore = -1;
 
-     $respiratoryScore = -1;
 
-     for ($x = 4; $x >= 0; $x--) {
+      for ($x = 4; $x >= 0; $x--) {
+         // get the value to compare against
          $comparator = $respiratoryRanges[$x][$respiratoryUnits];
-         echo $comparator;
+
+         // check for ventilation case
          if ($x > 2 && $isVentilated) {
             if ($respiratoryInput < $comparator) {
-                $respiratoryScore = $x;
-                break;
+               $respiratoryScore = $x;
+               break;
             }
-         } else if ($x > 2 && !$isVentilated) continue;
+         } 
+         // skip
+         else if ($x > 2 && !$isVentilated) continue;
+         // no ventilation
          else if ($x > 0){
             if ($respiratoryInput < $comparator) {
-                $respiratoryScore = $x;
-                break;
+               $respiratoryScore = $x;
+               break;
             }
+            // beyond 
          } else {
             if ($respiratoryInput >= $comparator) $respiratoryScore = $x;
          }
 
-     }
+      }
 
-     echo $respiratoryScore;
-    
-    
-    
-    $glasgowComa = array("15", "13-14", "10-12", "6-9", "<6");
-    
-    $cardiovascularScore = array("MAP ≥ 70 mmHg",
-    "MAP < 70 mmHg", "dopamine ≤ 5 μg/kg/min or dobutamine (any dose)",
-    "dopamine ≤ 5 μg/kg/min or dobutamine (any dose)",
-    "dopamine > 5 μg/kg/min OR epinephrine ≤ 0.1 μg/kg/min OR norepinephrine ≤ 0.1 μg/kg/min",
-    "dopamine > 15 μg/kg/min OR epinephrine > 0.1 μg/kg/min OR norepinephrine > 0.1 μg/kg/min"); 
+      return $respiratoryScore;
+  
+   }
 
-    $liverScore = array("< 1.2 [< 20.53]",
-    "1.2–1.9 [20-32]",
-    "2.0–5.9 [33-101]",
-    "6.0–11.9 [102-204]",
-    "> 12.0 [> 204]");
+   function getGlasgowComaInput() {
+      $glasgowComaOptions = array("15", "13-14", "10-12", "6-9", "<6");
+      return $glasgowComaOptions[$_POST["glasgow-coma"]];
+   }
+   
+   function getCardioVascularInput() {
+      $cardiovascularOptions = array("MAP ≥ 70 mmHg",
+      "MAP < 70 mmHg", "dopamine ≤ 5 μg/kg/min or dobutamine (any dose)",
+      "dopamine ≤ 5 μg/kg/min or dobutamine (any dose)",
+      "dopamine > 5 μg/kg/min OR epinephrine ≤ 0.1 μg/kg/min OR norepinephrine ≤ 0.1 μg/kg/min",
+      "dopamine > 15 μg/kg/min OR epinephrine > 0.1 μg/kg/min OR norepinephrine > 0.1 μg/kg/min"); 
 
-    $plateletScore = array("≥ 150", "< 150", "< 100", "< 50", "<20");
+      return $cardiovascularOptions[$_POST["cardiovascular-score"]];
+   }
+    
+   function getLiverInput() {
+      $liverOptions = array("< 1.2 [< 20.53]",
+      "1.2–1.9 [20-32]",
+      "2.0–5.9 [33-101]",
+      "6.0–11.9 [102-204]",
+      "> 12.0 [> 204]");
 
-    $creatinineScore = array("< 1.2 [< 110]",
-    "1.2–1.9 [110-170]",
-    "2.0–3.4 [171-299]",
-    "3.5–4.9 [300-440] (or < 500 ml/d)",
-    "> 5.0 [> 440] (or < 200 ml/d)");
+      return $liverOptions[$_POST["liver-score"]];
+   }
+    
+
+   function getPlateletInput() {
+      $plateletOptions = array("≥ 150", "< 150", "< 100", "< 50", "<20");
+
+      return $plateletOptions[$_POST["platelets-score"]];
+   }
+
+   function getCreatineInput() {
+      $creatinineScore = array("< 1.2 [< 110]",
+      "1.2–1.9 [110-170]",
+      "2.0–3.4 [171-299]",
+      "3.5–4.9 [300-440] (or < 500 ml/d)",
+      "> 5.0 [> 440] (or < 200 ml/d)");
+
+      return $creatinineScore[$_POST["creatinine-score"]];
+   }
+
+   function getTotalScore() {
+
+   }
 ?>
